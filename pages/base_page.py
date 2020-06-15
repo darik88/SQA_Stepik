3f0a8b22649ae4b346c0ed3c9373118d0ddac744
darik88 <mr.darik88@gmail.com>
2020-06-15 14:11:38 +0300

    login_page added

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,8 +1,19 @@
+from selenium.common.exceptions import NoSuchElementException
+
+
 class BasePage:
 
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, locator, element):
+        try:
+            self.browser.find_element(locator, element)
+        except NoSuchElementException:
+            return False
+        return True
